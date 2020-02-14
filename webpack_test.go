750ab5060867694c870d0c18403e0843d054b384
Chris Toshok <toshok@hound.sh>
2020-02-14 12:14:46 -0800

    add a test for images

--- webpack_test.go
+++ webpack_test.go
@@ -6,7 +6,8 @@ import (
 
 func TestManifestAssetHelper(t *testing.T) {
 	assets := map[string][]string{
-		"main.js": []string{"main.1.js", "main.2.js"},
+		"main.js":   []string{"main.1.js", "main.2.js"},
+		"image.png": []string{"image.3.png"},
 	}
 
 	tagHelper := createAssetTagHelper(&Config{
@@ -24,6 +25,17 @@ func TestManifestAssetHelper(t *testing.T) {
 	expectedHTML :=
 		`<script type="text/javascript" src="main.1.js"></script>
 <script type="text/javascript" src="main.2.js"></script>`
+
+	if string(html) != expectedHTML {
+		t.Fatalf("unexpected <script> tags\nexpected:\n%s\nactual:\n%s", expectedHTML, html)
+	}
+
+	html, err = tagHelper("image.png")
+	if err != nil {
+		t.Fatalf("error %v returned from asset tag helper for valid asset", err)
+	}
+	expectedHTML =
+		`<img src="image.3.png"></img>`
 
 	if string(html) != expectedHTML {
 		t.Fatalf("unexpected <script> tags\nexpected:\n%s\nactual:\n%s", expectedHTML, html)
@@ -53,7 +65,8 @@ func TestManifestAssetHelper(t *testing.T) {
 
 func TestManifestAssetHelperWithAssetHost(t *testing.T) {
 	assets := map[string][]string{
-		"main.js": []string{"main.1.js"},
+		"main.js":   []string{"main.1.js"},
+		"image.png": []string{"image.3.png"},
 	}
 
 	tagHelper := createAssetTagHelper(&Config{
@@ -75,6 +88,17 @@ func TestManifestAssetHelperWithAssetHost(t *testing.T) {
 
 	if string(html) != expectedHTML {
 		t.Fatalf("unexpected <script> tag\nexpected:\n%s\nactual:\n%s", expectedHTML, html)
+	}
+
+	html, err = tagHelper("image.png")
+	if err != nil {
+		t.Fatalf("error %v returned from asset tag helper for valid asset", err)
+	}
+	expectedHTML =
+		`<img src="//cdn.com/prefix/image.3.png"></img>`
+
+	if string(html) != expectedHTML {
+		t.Fatalf("unexpected <script> tags\nexpected:\n%s\nactual:\n%s", expectedHTML, html)
 	}
 
 	url, err := urlHelper("main.js")
